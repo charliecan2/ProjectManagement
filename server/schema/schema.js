5b3a4cd3e5addd280d8af2f8f5af5ec1f4833aed
Carlos Cantu <carloscq920@gmail.com>
2022-08-20 12:11:41 -0500

    Created the Client and Project object types and created a root query to export to the default index server file

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,48 @@
-const { buildSchema } = require('graphql');
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, buildSchema } = require('graphql');
+const { projects, clients } = require('../sampleData');
 
 const schema = buildSchema(`
   type Query {
     hello: String
   }
 `)
+
+// Client type
+const ClientType = new GraphQLObjectType({
+  name: 'Client',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString }
+  })
+})
+
+// Project type
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: GraphQLID },
+    clientID: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString }
+  })
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID }},
+      resolve(parent, args){
+        return clients.find(client => client.id === args.id)
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
