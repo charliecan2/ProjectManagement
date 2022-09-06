73896b312a0b27a779e851f92fa9f8201f89a12c
Carlos Cantu <carloscq920@gmail.com>
2022-09-06 12:05:07 -0500

    Created a Spinner component to show when the data is being fetched/loaded

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@apollo/client'
 import ClientRow from './ClientRow'
+import Spinner from './Spinner'
 import { GET_CLIENTS } from '../queries/clientQueries'
 
 
@@ -7,7 +8,7 @@ function Clients() {
   const { loading, error, data } = useQuery(GET_CLIENTS)
 
   if(loading){
-    return <p>Loading...</p>
+    return <Spinner />
   }
 
   if(error){
