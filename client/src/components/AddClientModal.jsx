6d34cb450aa5e55aaece508ef6e8ec9cf8ace70c
Carlos Cantu <carloscq920@gmail.com>
2022-09-08 16:57:05 -0500

    Modal was changed to a form, and can now submit data but only logs it onto the console for now

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -7,6 +7,12 @@ function AddClientsModal() {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
 
+  const submitClient = (e) => {
+    e.preventDefault();
+
+    console.log(name, email, phone)
+  }
+
   return (
     <div>
       <button type="button" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#addClientModal">
@@ -16,7 +22,7 @@ function AddClientsModal() {
         </div>
       </button>
 
-      <div className="modal fade" id="addClientModal" tabindex="-1" aria-labelledby="addClientModalLabel" aria-hidden="true">
+      <div className="modal fade" id="addClientModal" tabIndex="-1" aria-labelledby="addClientModalLabel" aria-hidden="true">
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
@@ -24,29 +30,32 @@ function AddClientsModal() {
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
-              <div className="mb-3">
-                <label className='from-label'>
-                  Name
-                </label>
-                <input type="text" className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)}/>
-              </div>
-              <div className="mb-3">
-                <label className='from-label'>
-                  Email
-                </label>
-                <input type="text" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-              </div>
-              <div className="mb-3">
-                <label className='from-label'>
-                  Phone
-                </label>
-                <input type="text" className="form-control" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)}/>
-              </div>
+              <form onSubmit={submitClient}>
+                <div className="mb-3">
+                  <label className='from-label'>
+                    Name
+                  </label>
+                  <input type="text" className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)}/>
+                </div>
+                <div className="mb-3">
+                  <label className='from-label'>
+                    Email
+                  </label>
+                  <input type="text" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+                <div className="mb-3">
+                  <label className='from-label'>
+                    Phone
+                  </label>
+                  <input type="text" className="form-control" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)}/>
+                </div>
+                <div className="modal-footer">
+                  <button type="submit" data-bs-dismiss="modal" className="btn btn-primary">Submit</button>
+                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                </div>
+              </form>
             </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-primary">Submit</button>
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-            </div>
+           
           </div>
         </div>
       </div>
